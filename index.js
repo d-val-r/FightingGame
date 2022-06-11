510efbd3013a006ea3e099815eab9edc2c180eb3
d-val-r <dvrudenya@noctrl.edu>
2022-06-10 21:08:23 -0500

    add jumping animations and fix double-jump bug

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ class Sprite {
         this.height = height;
         this.width = width;
         this.velocity = velocity;
+        this.crouch = false;
+        this.lastKey;
+        this.falling = false;
     }
 
     draw() {
@@ -24,22 +27,23 @@ class Sprite {
 
         // bounds on the y-axis
         if (this.position.y + this.height >= canvas.height) {
+            
+            // the player is no longer falling
+            this.falling = false;
             this.velocity.y = 0;
+
+            // if the sprite gets to high, simulate the jump ending
+            // by telling the program the user is no longer holding the jump button
+            // does not actually impact the user's ability to press the button
         } else if (this.position.y >= 300) {
             keys.w.pressed = false;
+            keys.upArrow.pressed = false;
         } else {
             this.velocity.y += GRAVITY;
         }
 
     }
 }
-
-
-// used to help control what happens when the user presses both a and d
-let lastKey;
-
-// boolean used to control crouching
-let crouch = false;
 
 
 // used to replicate effect of gravity on an object
@@ -81,7 +85,7 @@ canvas.height = 576;
 // render a rectangle at (0,0), filling the whole canvas
 ctx.fillRect(0,0, canvas.width, canvas.height);
 
-const player = new Sprite(
+const player1 = new Sprite(
     50, 
     150, 
     {
@@ -91,9 +95,9 @@ const player = new Sprite(
             {x: 0, y:0}
     });
 
-player.draw();
+player1.draw();
 
-const enemy = new Sprite(
+const player2 = new Sprite(
     50, 
     150, 
     {
@@ -103,7 +107,7 @@ const enemy = new Sprite(
             {x:0, y:0}
     });
 
-enemy.draw();
+player2.draw();
 
 
 // create an infinite animation loop that relies on the velocities 
@@ -113,61 +117,61 @@ const animate = () => {
     ctx.clearRect(0,0, canvas.width, canvas.height);
     ctx.fillStyle = 'black';
     ctx.fillRect(0,0, canvas.width, canvas.height);
-    player.update();
-    enemy.update();
+    player1.update();
+    player2.update();
 
     
     // player animation controls
 
     // horizontal controls
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -2;
-    } else if (keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 2;
+    if (keys.a.pressed && player1.lastKey === 'a') {
+        player1.velocity.x = -2;
+    } else if (keys.d.pressed && player1.lastKey === 'd') {
+        player1.velocity.x = 2;
     } else {
-        player.velocity.x = 0;
+        player1.velocity.x = 0;
     }
 
     // crouch and jump controls
-    if (keys.s.pressed && !crouch) {
-        player.height /= 2;
-        player.position.y += player.height;
-        crouch = true
-    } else if (!keys.s.pressed && crouch) {
-        player.height *= 2;
-        player.position.y -= player.height / 2;
-        crouch = false;
+    if (keys.s.pressed && !player1.crouch) {
+        player1.height /= 2;
+        player1.position.y += player1.height;
+        player1.crouch = true
+    } else if (!keys.s.pressed && player1.crouch) {
+        player1.height *= 2;
+        player1.position.y -= player1.height / 2;
+        player1.crouch = false;
     }
 
-    if (keys.w.pressed) {
-        player.velocity.y = -5;
+    if (keys.w.pressed && !player1.falling) {
+        player1.velocity.y = -5;
     } 
 
 
     // enemy animation controls
 
     // horizontal controls
-    if (keys.a.pressed && lastKey === 'a') {
-        player.velocity.x = -2;
-    } else if (keys.d.pressed && lastKey === 'd') {
-        player.velocity.x = 2;
+    if (keys.leftArrow.pressed && player2.lastKey === 'ArrowLeft') {
+        player2.velocity.x = -2;
+    } else if (keys.rightArrow.pressed && player2.lastKey === 'ArrowRight') {
+        player2.velocity.x = 2;
     } else {
-        player.velocity.x = 0;
+        player2.velocity.x = 0;
     }
 
     // crouch and jump controls
-    if (keys.s.pressed && !crouch) {
-        player.height /= 2;
-        player.position.y += player.height;
-        crouch = true
-    } else if (!keys.s.pressed && crouch) {
-        player.height *= 2;
-        player.position.y -= player.height / 2;
-        crouch = false;
+    if (keys.downArrow.pressed && !player2.crouch) {
+        player2.height /= 2;
+        player2.position.y += player2.height;
+        player2.crouch = true
+    } else if (!keys.downArrow.pressed && player2.crouch) {
+        player2.height *= 2;
+        player2.position.y -= player2.height / 2;
+        player2.crouch = false;
     }
 
-    if (keys.w.pressed) {
-        player.velocity.y = -5;
+    if (keys.upArrow.pressed && !player2.falling) {
+        player2.velocity.y = -5;
     } 
 
 }
@@ -178,17 +182,36 @@ window.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'd':
             keys.d.pressed = true;
-            lastKey = 'd';
+            player1.lastKey = 'd';
             break
         case 'a':
             keys.a.pressed = true;
-            lastKey = 'a';
+            player1.lastKey = 'a';
             break
         case 's':
             keys.s.pressed = true;
             break;
         case 'w':
             keys.w.pressed = true;
+
+            // set the sprite's falling variable to true; that way, the user cannot
+            // keep clicking 'w' (or up arrow) to "double jump"
+            player1.falling = true;
+            break;
+        case 'ArrowRight':
+            keys.rightArrow.pressed = true;
+            player2.lastKey = 'ArrowRight';
+            break
+        case 'ArrowLeft':
+            keys.leftArrow.pressed = true;
+            player2.lastKey = 'ArrowLeft';
+            break
+        case 'ArrowDown':
+            keys.downArrow.pressed = true;
+            break;
+        case 'ArrowUp':
+            keys.upArrow.pressed = true;
+            player2.falling = true;
             break;
     }
 });
@@ -202,17 +225,31 @@ window.addEventListener('keyup', (event) => {
             
             // reset the last key to the opposite key so that if
             // both keys are held the character will move in the correct direction
-            lastKey = 'a';
+            player1.lastKey = 'a';
             break
         case 'a':
             keys.a.pressed = false;
-            lastKey = 'd';
+            player1.lastKey = 'd';
             break
         case 's':
             keys.s.pressed = false;
             break;
         case 'w':
             keys.w.pressed = false;
+            break;
+        case 'ArrowRight':
+            keys.rightArrow.pressed = false;
+            player2.lastKey = 'ArrowLeft';
+            break
+        case 'ArrowLeft':
+            keys.leftArrow.pressed = false;
+            player2.lastKey = 'ArrowRight';
+            break
+        case 'ArrowDown':
+            keys.downArrow.pressed = false;
+            break;
+        case 'ArrowUP':
+            keys.upArrow.pressed = false;
             break;
     }
 });
