bc62465bc3c083b9e46d0e1982c041b32b1e4062
d-val-r <dvrudenya@noctrl.edu>
2022-06-13 21:58:03 -0500

    implement attack box model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Represents the sprite rendered on the canvas
 class Sprite {
-    constructor(width, height, {position, velocity}) {
+    constructor(width, height, {position, velocity}, direction) {
         this.position = position;
         this.height = height;
         this.width = width;
@@ -8,17 +8,40 @@ class Sprite {
         this.crouch = false;
         this.lastKey;
         this.falling = false;
+        this.direction = direction
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y
+            },
+            width: 100,
+            height: width
+        }
     }
 
     draw() {
+        
+        // draw the sprite
         ctx.fillStyle = 'red';
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        // draw the attackbox
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
     }
 
     update() {
         this.draw();
+        
+        // update the sprite's position
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+
+        // update the position of the attackbox
+        // reverse the direction of the attack box based on the direction the
+        // sprite is facing
+        this.attackBox.position.x = this.position.x - (this.direction === "left" ? this.width : 0);
+        this.attackBox.position.y = this.position.y;
         
         // bounds on the x-axis
         if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
@@ -93,7 +116,8 @@ const player1 = new Sprite(
             {x:0, y:canvas.height - 150}, 
         velocity: 
             {x: 0, y:0}
-    });
+    },
+    "right");
 
 player1.draw();
 
@@ -105,7 +129,8 @@ const player2 = new Sprite(
             {x:canvas.width - 50, y:canvas.height - 150}, 
         velocity: 
             {x:0, y:0}
-    });
+    }, 
+    "left");
 
 player2.draw();
 
